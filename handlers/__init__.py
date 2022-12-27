7a9171ad03c8e811bf7f2363d8392451a93d4045
VLAD A <apukhtin00@inbox.ru>
2022-12-27 19:59:04 +0300

    1

---
+++ handlers/__init__.py
@@ -0,0 +1,3 @@
+from handlers import client
+from handlers import admin
+from keyboards.client_kb import kb_client
--- info_bot.py
+++ info_bot.py
@@ -1,1 +1,11 @@
-print('hello')
+from aiogram.utils import executor
+from create_bot import dp
+from handlers import client, admin
+
+async def on_startup(_):
+    print("bot is online now")
+
+client.register_handlers_client(dp)
+
+
+executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
